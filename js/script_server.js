29815ee288c93fc0c14011788c1ec8e43112230a
Tollic <72671011+Tollic@users.noreply.github.com>
2023-06-14 21:59:11 +0300

    Add files via upload

--- js/script_server.js
+++ js/script_server.js
@@ -57,72 +57,120 @@
 let counter = 1;
 let previousTextBlock = null;
 let previousIndentedTextBlock = null;
+let total = 0;
+let sum = 0;
 
 function addText() {
-    const content = document.getElementById('content').value;
-    const counterElement = document.getElementById('cyp_bon');
-  
-    const textBlock = document.createElement('div');
-    textBlock.classList.add('text-block', 'text-block-left');
-  
-    const numberedContent = content;
-    textBlock.textContent = numberedContent;
-  
-    const deleteButton = document.createElement('span');
-    deleteButton.classList.add('delete-button');
-    deleteButton.textContent = '✖';
-  
-    deleteButton.addEventListener('click', function() {
-      textBlock.remove();
-    });
-  
-    textBlock.appendChild(deleteButton);
-  
-    const textContainer_left = document.getElementById('textContainer_left');
-    textContainer_left.appendChild(textBlock);
-  
-    document.getElementById('content').value = '';
-  
-    textBlock.style.background = '#3d74b8';
-  
-    if (previousTextBlock !== null) {
-      previousTextBlock.style.background = '';
+  const content = document.getElementById('content').value;
+  const counterElement = document.getElementById('cyp_bon');
+
+  const textBlock = document.createElement('div');
+  textBlock.classList.add('text-block', 'text-block-left');
+  textBlock.textContent = content;
+
+  textBlock.addEventListener('click', function () {
+    if (!textBlock.isEditing) {
+      const input = document.createElement('input');
+      input.type = 'text';
+      input.value = textBlock.textContent;
+      textBlock.textContent = '';
+      textBlock.appendChild(input);
+      input.focus();
+      textBlock.isEditing = true;
+
+      input.addEventListener('blur', function () {
+        textBlock.textContent = input.value;
+        textBlock.isEditing = false;
+      });
     }
-  
-    previousTextBlock = textBlock;
+  });
+
+  const deleteButton = document.createElement('span');
+  deleteButton.addEventListener('click', function () {
+    textBlock.remove();
+  });
+
+  textBlock.appendChild(deleteButton);
+
+  const textContainer_left = document.getElementById('textContainer_left');
+  textContainer_left.appendChild(textBlock);
+
+  document.getElementById('content').value = '';
+
+  textBlock.style.background = '#3d74b8';
+
+  if (previousTextBlock !== null) {
+    previousTextBlock.style.background = '';
   }
-  
+
+  previousTextBlock = textBlock;
+}
+
 
 function addIndentedText() {
-    const contentIndented = document.getElementById('contentIndented').value;
-  
-    const textBlockIndented = document.createElement('div');
-    textBlockIndented.classList.add('text-block', 'text-block-indented');
-  
-    const numberedContent = contentIndented;
-    textBlockIndented.textContent = numberedContent;
-  
-    const deleteButton = document.createElement('span');
-    deleteButton.classList.add('delete-button');
-    deleteButton.textContent = '✖';
-  
-    deleteButton.addEventListener('click', function() {
-      textBlockIndented.remove();
-    });
-  
-    textBlockIndented.appendChild(deleteButton);
-  
-    const textContainer_rignt = document.getElementById('textContainer_rignt');
-    textContainer_rignt.appendChild(textBlockIndented);
-  
-    document.getElementById('contentIndented').value = '';
-  
-    textBlockIndented.style.background = '#3d74b8';
-  
-    if (previousIndentedTextBlock !== null) {
-      previousIndentedTextBlock.style.background = '';
+  const contentIndented = document.getElementById('contentIndented').value;
+
+  const textBlockIndented = document.createElement('div');
+  textBlockIndented.classList.add('text-block', 'text-block-indented');
+  textBlockIndented.textContent = contentIndented;
+
+  textBlockIndented.addEventListener('click', function () {
+    if (!textBlockIndented.isEditing) {
+      const input = document.createElement('input');
+      input.type = 'text';
+      input.value = textBlockIndented.textContent;
+      textBlockIndented.textContent = '';
+      textBlockIndented.appendChild(input);
+      input.focus();
+      textBlockIndented.isEditing = true;
+
+      input.addEventListener('blur', function () {
+        textBlockIndented.textContent = input.value;
+        textBlockIndented.isEditing = false;
+        updateSum();
+      });
     }
-  
-    previousIndentedTextBlock = textBlockIndented;
+  });
+
+  const deleteButton = document.createElement('span');
+  deleteButton.addEventListener('click', function () {
+    textBlockIndented.remove();
+    updateSum();
+  });
+
+  textBlockIndented.appendChild(deleteButton);
+
+  const textContainer_rignt = document.getElementById('textContainer_rignt');
+  textContainer_rignt.appendChild(textBlockIndented);
+
+  document.getElementById('contentIndented').value = '';
+
+  textBlockIndented.style.background = '#3d74b8';
+
+  if (previousIndentedTextBlock !== null) {
+    previousIndentedTextBlock.style.background = '';
+  }
+
+  previousIndentedTextBlock = textBlockIndented;
+
+  updateSum();
 }
-  
+
+function updateSum() {
+  sum = 0;
+  const textBlocks = document.querySelectorAll('.text-block-indented');
+  textBlocks.forEach(function (textBlock) {
+    const content = textBlock.textContent.trim();
+    const regex = /=\s*(\d+)/;
+    const match = regex.exec(content);
+    if (match) {
+      const number = parseInt(match[1]);
+      if (!isNaN(number)) {
+        sum += number;
+      }
+    }
+  });
+
+  const playedElement = document.getElementById('played');
+  playedElement.textContent = isNaN(sum) ? '0' : sum.toString();
+}
